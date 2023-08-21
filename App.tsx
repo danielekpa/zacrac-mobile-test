23dababc6a059b5e49f0a4516261037b6be57329
Daniel Ekpa <ekpa_d@yahoo.com>
2023-08-21 02:01:01 +0100

    Initial setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+import { useFonts } from 'expo-font';
+import { Inter_400Regular } from '@expo-google-fonts/inter';
+import { DMSans_400Regular, DMSans_700Bold } from '@expo-google-fonts/dm-sans';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded, fontError] = useFonts({
+		Inter_400Regular,
+		DMSans_400Regular,
+		DMSans_700Bold,
+		Averta: require('./assets/fonts/AvertaDemo-Regular.otf'),
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	SplashScreen.hideAsync();
+
+	return (
+		<SafeAreaProvider>
+			<View style={styles.container}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style='auto' />
+			</View>
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
